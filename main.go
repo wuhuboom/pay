bbcd622ebff8fda7d8f16fb775b7b10b7eeb82d9
wuhuboom <wuhuboom99@gmail.com>
2022-12-17 15:02:16 +0800

    sj2

--- main.go
+++ main.go
@@ -14,16 +14,3 @@ func main() {
 	cmd.Execute()
 
 }
-
-//func GET() {
-//	priv, err := goencryption.GenPrvKey(2048)
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//	fmt.Printf("%s\n", priv)
-//	pub, err := goencryption.GenPubKeyFromPrvKey(priv)
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//	fmt.Printf("%s\n", pub)
-//}
